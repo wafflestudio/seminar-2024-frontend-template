6eee7643144e92c773396eb743d73775f4ac432b
woohm402 <woohm404@gmail.com>
2024-09-24 02:02:37 +0900

    최신화

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import react from '@woohm402/eslint-config-react';
 
 export default [
-  { ignores: ['eslint.config.js', '.yarn'] },
+  {
+    ignores: ['.yarn', '*.js'],
+  },
   ...react({
     tsconfigRootDir: import.meta.dirname,
   }),
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import './reset.css';
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import { App } from './App';
 
 const root = document.getElementById('root');
 
